74ea5bd81f7b4583ef14e25f711fea21f376e662
Simon Lees <simon@simotek.net>
2016-02-18 21:37:03 +1030

    Add color module

--- config.fish
+++ config.fish
@@ -2,8 +2,9 @@
 
 set tacklebox_path ~/src/config/tackle
 set tacklebox_modules virtualfish virtualhooks
-set tacklebox_plugins python extract
+set tacklebox_plugins python extract find-in colors
 set tacklebox_theme simotek
 
 #initialise tacklebox
 source ~/src/config/tacklebox/tacklebox.fish
+set PYTHONPATH $PYTHONPATH:/opt/python/lib/python2.7/site-packages
