879684fe8bb47047aff9ed4fb98968ff899ee33a
Simon Lees <sflees@suse.de>
2016-06-03 15:04:41 +0930

    Update for my latest changes to tackle
    * Environment is now handled by tackle
    * grc and devel plugins should be enabled
    * no need to call colors command

--- config.fish
+++ config.fish
@@ -2,9 +2,8 @@
 
 set tacklebox_path ~/src/config/tackle
 set tacklebox_modules virtualfish virtualhooks
-set tacklebox_plugins python extract find-in colors
+set tacklebox_plugins python extract find-in colors grc devel
 set tacklebox_theme simotek
 
 #initialise tacklebox
 source ~/src/config/tacklebox/tacklebox.fish
-set PYTHONPATH $PYTHONPATH:/opt/python/lib/python2.7/site-packages
